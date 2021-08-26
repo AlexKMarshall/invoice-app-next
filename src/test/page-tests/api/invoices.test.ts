3ad5b643a14b6c40e01caa11846273f1ff89feae
Alex Marshall <alex.k.marshall83@gmail.com>
2021-08-26 18:22:32 +0100

    test: add basic e2e test for POST /api/invoices
    Doesn't yet connect to persistance

--- src/test/page-tests/api/invoices.test.ts
+++ src/test/page-tests/api/invoices.test.ts
@@ -1,3 +1,4 @@
+import { buildMockDraftInvoiceInput } from 'src/server/test/mocks/invoice.fixtures'
 import handler from 'src/pages/api/invoices'
 import { testApiHandler } from 'next-test-api-route-handler'
 
@@ -13,6 +14,36 @@ it('should get invoices', async () => {
       expect(data).toMatchObject({
         data: {
           invoices: expect.any(Array),
+        },
+      })
+    },
+  })
+})
+
+it('should post a draft invoice', async () => {
+  expect.hasAssertions()
+
+  await testApiHandler({
+    handler,
+    test: async ({ fetch }) => {
+      const data = buildMockDraftInvoiceInput()
+
+      const response = await fetch({
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+
+      const result = await response.json()
+
+      expect(result).toMatchObject({
+        data: {
+          savedInvoice: {
+            ...JSON.parse(JSON.stringify(data)),
+            id: expect.stringMatching(/[A-Z]{2}\d{4}/),
+          },
         },
       })
     },
