d6292f2aa16fa28ac2cb02a24ef05740f6dd5f47
Alex Marshall <alex.k.marshall83@gmail.com>
2021-08-29 09:00:30 +0100

    feat(invoicecontroller): validate invoice form data

--- src/server/features/invoice/invoice.controller.test.ts
+++ src/server/features/invoice/invoice.controller.test.ts
@@ -4,6 +4,7 @@ import * as invoiceModel from './invoice.model'
 import { NewInvoiceInputDTO, Stringify } from 'src/shared/dtos'
 import {
   buildMockDraftInvoiceInput,
+  buildMockInvoiceInput,
   buildMockInvoiceSummary,
 } from '../../test/mocks/invoice.fixtures'
 
@@ -94,6 +95,99 @@ describe('postInvoice', () => {
     expect(result.code).toBe(500)
     expect(result.response).toEqual({
       error: JSON.stringify(mockError),
+    })
+  })
+  it('should return error when input has empty strings', async () => {
+    const mockInput = buildMockInvoiceInput({
+      clientName: '',
+      clientEmail: '',
+      clientAddress: {
+        street: '',
+        city: '',
+        postcode: '',
+        country: '',
+      },
+      senderAddress: {
+        street: '',
+        city: '',
+        postcode: '',
+        country: '',
+      },
+      projectDescription: '',
+      itemList: [{ name: '' }],
+    })
+    const dtoInput = JSON.parse(JSON.stringify(mockInput))
+
+    const result = await invoiceController.postInvoice(dtoInput)
+
+    expect(result.code).toBe(400)
+    expect(result.response).toEqual({
+      error: {
+        fieldErrors: {
+          clientName: ['Should be at least 1 characters'],
+          clientEmail: ['Invalid email'],
+          clientAddress: {
+            street: ['Should be at least 1 characters'],
+            city: ['Should be at least 1 characters'],
+            country: ['Should be at least 1 characters'],
+            postcode: ['Should be at least 1 characters'],
+          },
+          senderAddress: {
+            street: ['Should be at least 1 characters'],
+            city: ['Should be at least 1 characters'],
+            country: ['Should be at least 1 characters'],
+            postcode: ['Should be at least 1 characters'],
+          },
+          projectDescription: ['Should be at least 1 characters'],
+          itemList: {
+            '0': {
+              name: ['Should be at least 1 characters'],
+            },
+          },
+        },
+      },
+    })
+  })
+
+  it('should return error on invalid numbers', async () => {
+    const mockInput = buildMockInvoiceInput({
+      paymentTerms: -1,
+      itemList: [{ quantity: 0, price: -1 }],
+    })
+    const dtoInput = JSON.parse(JSON.stringify(mockInput))
+
+    const result = await invoiceController.postInvoice(dtoInput)
+
+    expect(result.code).toBe(400)
+    expect(result.response).toEqual({
+      error: {
+        fieldErrors: {
+          paymentTerms: ['Value should be greater than or equal to 0'],
+          itemList: {
+            '0': {
+              quantity: ['Value should be greater than or equal to 1'],
+              price: ['Value should be greater than or equal to 0'],
+            },
+          },
+        },
+      },
+    })
+  })
+  it('should return error on invalid email', async () => {
+    const mockInput = buildMockInvoiceInput({
+      clientEmail: 'not an email',
+    })
+    const dtoInput = JSON.parse(JSON.stringify(mockInput))
+
+    const result = await invoiceController.postInvoice(dtoInput)
+
+    expect(result.code).toBe(400)
+    expect(result.response).toEqual({
+      error: {
+        fieldErrors: {
+          clientEmail: ['Invalid email'],
+        },
+      },
     })
   })
 })
--- src/server/features/invoice/invoice.controller.ts
+++ src/server/features/invoice/invoice.controller.ts
@@ -1,11 +1,12 @@
 import * as invoiceModel from './invoice.model'
+import * as z from 'zod'
 
 import {
   GetInvoiceSummaryDTO,
   NewInvoiceInputDTO,
   NewInvoiceReturnDTO,
-  Stringify,
 } from 'src/shared/dtos'
+import { JsonArray, JsonObject } from 'type-fest'
 
 import { parseJSON } from 'date-fns'
 
@@ -36,15 +37,21 @@ export function getInvoices(): ControllerResponse<GetInvoiceSummaryDTO> {
 }
 
 export function postInvoice(
-  newInvoice: Stringify<NewInvoiceInputDTO>
+  newInvoice: JsonObject | JsonArray
 ): ControllerResponse<NewInvoiceReturnDTO> {
-  const invoiceWithDates = {
-    ...newInvoice,
-    issuedAt: parseJSON(newInvoice.issuedAt),
+  const parsingResult = parseNewInvoiceInputDto(newInvoice)
+
+  if (!parsingResult.success) {
+    return Promise.resolve({
+      code: 400,
+      response: { error: parsingResult.error },
+    })
   }
 
+  const parsedInvoice = parsingResult.data
+
   return invoiceModel
-    .create(invoiceWithDates)
+    .create(parsedInvoice)
     .then((savedInvoice) => ({
       code: 201,
       response: { data: { savedInvoice } },
@@ -53,4 +60,96 @@ export function postInvoice(
       code: 500,
       response: { error: JSON.stringify(error) },
     }))
+}
+
+type SafeParse<T> =
+  | { success: true; data: T }
+  | {
+      success: false
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      error: any
+    }
+
+function parseNewInvoiceInputDto(
+  newInvoiceInput: JsonObject | JsonArray
+): SafeParse<NewInvoiceInputDTO> {
+  const buildingInvoice = { ...newInvoiceInput }
+
+  if (
+    'issuedAt' in buildingInvoice &&
+    typeof buildingInvoice.issuedAt === 'string'
+  ) {
+    Object.assign(buildingInvoice, {
+      issuedAt: parseJSON(buildingInvoice.issuedAt),
+    })
+  }
+
+  const result = newInvoiceInputDtoSchema.safeParse(buildingInvoice)
+
+  if (result.success === false) {
+    return { ...result, error: flattenError(result.error) }
+  }
+
+  return result
+}
+
+function schemaForType<T>() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return <S extends z.ZodType<T, any, any>>(arg: S) => arg
+}
+
+const addressSchema = z.object({
+  street: z.string().min(1),
+  city: z.string().min(1),
+  country: z.string().min(1),
+  postcode: z.string().min(1),
+})
+
+const newInvoiceInputDtoSchema = schemaForType<NewInvoiceInputDTO>()(
+  z.object({
+    status: z.enum(['draft', 'pending', 'paid']),
+    senderAddress: addressSchema,
+    clientName: z.string().min(1),
+    clientEmail: z.string().email(),
+    clientAddress: addressSchema,
+    issuedAt: z.date(),
+    paymentTerms: z.number().min(0),
+    projectDescription: z.string().min(1),
+    itemList: z.array(
+      z.object({
+        name: z.string().min(1),
+        quantity: z.number().min(1),
+        price: z.number().min(0),
+      })
+    ),
+  })
+)
+
+function flattenError(
+  error: z.ZodError<z.infer<typeof newInvoiceInputDtoSchema>>
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const fieldErrors: any = {}
+  for (const issue of error.issues) {
+    if (issue.path.length > 0) {
+      if (issue.path.length === 1) {
+        fieldErrors[issue.path[0]] ??= []
+        fieldErrors[issue.path[0]].push(issue.message)
+      } else if (issue.path.length === 2) {
+        fieldErrors[issue.path[0]] ??= {}
+        fieldErrors[issue.path[0]][issue.path[1]] ??= []
+        fieldErrors[issue.path[0]][issue.path[1]].push(issue.message)
+      } else {
+        fieldErrors[issue.path[0]] ??= {}
+        fieldErrors[issue.path[0]][issue.path[1]] ??= {}
+        fieldErrors[issue.path[0]][issue.path[1]][issue.path[2]] ??= []
+        fieldErrors[issue.path[0]][issue.path[1]][issue.path[2]].push(
+          issue.message
+        )
+      }
+    } else {
+      // form error
+    }
+  }
+  return { fieldErrors }
 }
