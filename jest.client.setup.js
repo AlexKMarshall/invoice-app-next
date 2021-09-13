06cb5a3ea6397d0f9f4b17fd27aa50952b8a5f67
Alex Marshall <alex.k.marshall83@gmail.com>
2021-09-13 08:13:34 +0100

    feat: install vanilla-extract

--- jest.client.setup.js
+++ jest.client.setup.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import 'whatwg-fetch'
 import 'jest-extended'
+import '@vanilla-extract/css/disableRuntimeStyles'
 
 import { server } from './src/client/test/mocks/server'
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
+
+const nextConfig = {
   reactStrictMode: true,
 }
+
+module.exports = withVanillaExtract(nextConfig)
