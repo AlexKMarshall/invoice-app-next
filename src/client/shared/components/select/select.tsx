8e7332d169336b2520f11b71e820cc99a2a19f67
Alex Marshall <alex.k.marshall83@gmail.com>
2021-10-01 01:39:47 +0100

    fix: make payment term required in the invoice form

--- src/client/shared/components/select/select.tsx
+++ src/client/shared/components/select/select.tsx
@@ -1,17 +1,35 @@
 import { ForwardedRef, forwardRef } from 'react'
+import {
+  OptionHTMLAttributes,
+  SelectHTMLAttributes,
+} from 'hoist-non-react-statics/node_modules/@types/react'
 import { labelWrapper, select, wrapper } from './select.css'
 
-import { SelectHTMLAttributes } from 'hoist-non-react-statics/node_modules/@types/react'
 import { useId } from '@react-aria/utils'
 
 type Props = {
   label: string
   isLoading?: boolean
-  options?: Array<{ value: string; label?: string }>
+  options?: Array<Option>
   errorMessage?: string
 } & SelectHTMLAttributes<HTMLSelectElement>
 
-const loadingOption = { value: '', label: 'Loading...' }
+type Option = {
+  value: string
+  label?: string
+  optionProps?: OptionHTMLAttributes<HTMLOptionElement>
+}
+
+const loadingOption = {
+  value: '',
+  label: 'Loading...',
+  optionProps: { disabled: true },
+}
+const emptyOption = {
+  value: '',
+  label: '',
+  optionProps: { disabled: true },
+}
 
 export const Select = forwardRef(function Select(
   {
@@ -20,11 +38,12 @@ export const Select = forwardRef(function Select(
     options: optionsProp = [],
     errorMessage,
     className,
+    defaultValue = '',
     ...delegatedProps
   }: Props,
   ref: ForwardedRef<HTMLSelectElement>
 ): JSX.Element {
-  const options = isLoading ? [loadingOption] : optionsProp
+  const options = isLoading ? [loadingOption] : [emptyOption, ...optionsProp]
   const controlId = useId()
   const errorId = useId()
 
@@ -47,12 +66,17 @@ export const Select = forwardRef(function Select(
         className={select}
         id={controlId}
         ref={ref}
+        defaultValue={defaultValue}
         {...delegatedProps}
         aria-invalid={status === 'error'}
         aria-describedby={status === 'error' ? errorId : undefined}
       >
         {options.map((option) => (
-          <option key={option.value} value={option.value}>
+          <option
+            key={option.value}
+            value={option.value}
+            {...option.optionProps}
+          >
             {option.label ?? option.value}
           </option>
         ))}
